13d009feb5e297207d9ceae6a36c4f71441d3cc2
“Evgeny <ahotickkk@mail.ru>
2021-07-29 11:36:36 +0300

    update project

--- dist/clak-online-ui-kit.80dfb952.js
+++ dist/clak-online-ui-kit.80dfb952.js
@@ -12865,7 +12865,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58991" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61810" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
