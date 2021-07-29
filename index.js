982a474cb00f89b3e5e64e1b204e5534fce53468
“Evgeny <ahotickkk@mail.ru>
2021-07-29 18:08:50 +0300

    update project

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import './src/main.scss'
 import './src/js/chart.js';
 
 import ValveControllerTabs from './src/js/valve-controller-tabs.js';
-import ChartWaterFlowMetr from './src/js/chart-waterflow-metr.js';
-import ChartWaterFlow from './src/js/chart-water-flow.js';
-import ChartResource from './src/js/chart-resource.js';
-import ChartReagent from './src/js/chart-reagent.js';
 import CustomizationTabs from './src/js/customization-tabs.js';
 import StatisticsTabs from './src/js/statistics-tabs.js';
 import IntervalTabs from './src/js/interval-tabs.js';
@@ -16,13 +12,13 @@ import Tooltip from './src/js/tooltip.js';
 import Form from './src/js/form.js';
 import Tabs from './src/js/tabs.js';
 
+import ChartWaterFlowMetr from './src/js/chart-waterflow-metr.js';
+import ChartWaterFlow from './src/js/chart-water-flow.js';
+import ChartResource from './src/js/chart-resource.js';
+import ChartReagent from './src/js/chart-reagent.js';
+
 document.addEventListener( 'DOMContentLoaded', event => {
     new ValveControllerTabs;
-    new ChartWaterFlowMetr;
-    new CustomizationTabs;
-    new ChartWaterFlow;
-    new ChartResource;
-    new ChartReagent;
     new StatisticsTabs;
     new IntervalTabs;
     new ValvesTabs;
@@ -31,4 +27,10 @@ document.addEventListener( 'DOMContentLoaded', event => {
     new Tooltip;
     new Form;
     new Tabs;
+
+    new ChartWaterFlowMetr;
+    new CustomizationTabs;
+    new ChartWaterFlow;
+    new ChartResource;
+    new ChartReagent;
 });
