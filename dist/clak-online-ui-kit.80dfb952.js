632df7d9e631bcde7f959575e8db7215603be224
“Evgeny <ahotickkk@mail.ru>
2021-08-03 14:26:02 +0300

    update all project

--- dist/clak-online-ui-kit.80dfb952.js
+++ dist/clak-online-ui-kit.80dfb952.js
@@ -11609,8 +11609,7 @@ exports.default = void 0;
 
 class ChartResource {
   constructor() {
-    this.chart = document.getElementById("chart-resource");
-    this.init();
+    this.chart = document.getElementById("chart-resource"); // this.init();
   }
 
   init() {
@@ -12918,7 +12917,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55092" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59630" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
